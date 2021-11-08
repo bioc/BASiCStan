cc4682973aefe410c72d5bc893464b99509a5e07
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2021-11-08 21:23:07 +0000

    Accuracy tests

--- tests/testthat/test-basics-stan.R
+++ tests/testthat/test-basics-stan.R
@@ -1,7 +1,7 @@
-set.seed(42)
-mock <- BASiCS::BASiCS_MockSCE(NGenes = 5, NCells = 5, NSpikes = 5)
+set.seed(31)
+mock <- BASiCS::BASiCS_MockSCE(NGenes = 20, NCells = 20, NSpikes = 10)
 
-test_that("stan works in basic modes", {
+test_that("smoke test", {
     expect_s4_class(
         suppressWarnings(BASiCStan(mock, tol_rel_obj = 1, Verbose = FALSE)),
         "BASiCS_Chain"
@@ -50,4 +50,63 @@ test_that("stan works in basic modes", {
         "BASiCS_Chain"
     )
 
+})
+
+set.seed(66)
+mock <- BASiCS::BASiCS_MockSCE(NGenes = 20, NCells = 20, NSpikes = 10)
+
+test_that("accuracy test", {
+    a <- suppressMessages(capture.output(
+        fitb <- BASiCS_MCMC(
+            mock, N = 20000, Thin = 10, Burn = 10000, Regression = TRUE
+        )
+    ))
+    fit1 <- suppressWarnings(
+        BASiCStan(mock, tol_rel_obj = 1e-3, Verbose = FALSE)
+    )
+    fit2 <- suppressWarnings(
+        BASiCStan(mock, WithSpikes = FALSE, tol_rel_obj = 1e-3, Verbose = FALSE)
+    )
+    expect_equal(
+        colMedians(fitb@parameters$mu), colMedians(fit1@parameters$mu),
+        tolerance = 1e-1
+    )
+    expect_gt(
+        cor(
+            colMedians(fitb@parameters$delta),
+            colMedians(fit1@parameters$delta),
+            use = "complete.obs"
+        ),
+        0.7
+    )
+    expect_gt(
+        cor(
+            colMedians(fitb@parameters$epsilon),
+            colMedians(fit1@parameters$epsilon),
+            use = "complete.obs"
+        ),
+        0.6
+    )
+    ## no spikes needs offset correcting
+    fit2 <- BASiCS:::.offset_correct(fit2, fitb)
+    expect_equal(
+        colMedians(fitb@parameters$mu), colMedians(fit2@parameters$mu),
+        tolerance = 1e-1
+    )
+    expect_gt(
+        cor(
+            colMedians(fitb@parameters$delta),
+            colMedians(fit2@parameters$delta),
+            use = "complete.obs"
+        ),
+        0.7
+    )
+    expect_gt(
+        cor(
+            colMedians(fitb@parameters$epsilon),
+            colMedians(fit2@parameters$epsilon),
+            use = "complete.obs"
+        ),
+        0.7
+    )
 })
